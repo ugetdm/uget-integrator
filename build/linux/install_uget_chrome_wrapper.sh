7ebbc258b680f6944c25de53cb7c0178ddc42dae
Gobinath <slgobinath@gmail.com>
2017-07-31 10:38:15 -0400

    Add Linux installation scripts

---
+++ build/linux/install_uget_chrome_wrapper.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+echo "Installing uget-chrome-wrapper 2.0.4"
+
+# Download uget-chrome-wrapper to /usr/bin/uget-chrome-wrapper
+sudo wget https://raw.githubusercontent.com/slgobinath/uget-chrome-wrapper/v2.0.4/uget-chrome-wrapper/bin/uget-chrome-wrapper --output-document /usr/bin/uget-chrome-wrapper
+
+# Make the uget-chrome-wrapper executable
+sudo chmod +x /usr/bin/uget-chrome-wrapper
+
+# If you don't have Chromium, Google Chrome, Vivaldi or Opera, you can comment the gollowing two lines
+########################################################### Chromium, Google Chrome, Vivaldi and Opera ##########################################################
+# Create the required directories for native messaging host configuration
+sudo mkdir -p /etc/opt/chrome/native-messaging-hosts
+sudo mkdir -p /etc/chromium/native-messaging-hosts
+sudo mkdir -p /etc/opera/native-messaging-hosts
+
+# Download com.javahelps.ugetchromewrapper.json to /etc/opt/chrome/native-messaging-hosts/com.javahelps.ugetchromewrapper.json
+sudo wget https://raw.githubusercontent.com/slgobinath/uget-chrome-wrapper/v2.0.4/uget-chrome-wrapper/conf/com.javahelps.ugetchromewrapper.json --output-document /etc/opt/chrome/native-messaging-hosts/com.javahelps.ugetchromewrapper.json
+
+# Download com.javahelps.ugetchromewrapper.json to /etc/chromium/native-messaging-hosts/com.javahelps.ugetchromewrapper.json
+sudo wget https://raw.githubusercontent.com/slgobinath/uget-chrome-wrapper/v2.0.4/uget-chrome-wrapper/conf/com.javahelps.ugetchromewrapper.json --output-document /etc/chromium/native-messaging-hosts/com.javahelps.ugetchromewrapper.json
+
+# Download com.javahelps.ugetchromewrapper.json to /etc/opera/native-messaging-hosts/com.javahelps.ugetchromewrapper.json
+sudo wget https://raw.githubusercontent.com/slgobinath/uget-chrome-wrapper/v2.0.4/uget-chrome-wrapper/conf/com.javahelps.ugetchromewrapper.json --output-document /etc/opera/native-messaging-hosts/com.javahelps.ugetchromewrapper.json
+
+# If you don't have Firefox, you can comment the gollowing two lines
+######################################################################## Mozilla Firefox ########################################################################
+# Create the required directories for native messaging host configuration
+sudo mkdir -p /usr/lib/mozilla/native-messaging-hosts
+sudo mkdir -p /usr/lib64/mozilla/native-messaging-hosts
+# Download com.javahelps.ugetfirefoxwrapper.json to /usr/lib/mozilla/native-messaging-hosts/com.javahelps.ugetfirefoxwrapper.json
+sudo wget https://raw.githubusercontent.com/slgobinath/uget-chrome-wrapper/v2.0.4/uget-chrome-wrapper/conf/com.javahelps.ugetfirefoxwrapper.json --output-document /usr/lib/mozilla/native-messaging-hosts/com.javahelps.ugetfirefoxwrapper.json
+
+# Download com.javahelps.ugetfirefoxwrapper.json to /usr/lib64/mozilla/native-messaging-hosts/com.javahelps.ugetfirefoxwrapper.json
+sudo wget https://raw.githubusercontent.com/slgobinath/uget-chrome-wrapper/v2.0.4/uget-chrome-wrapper/conf/com.javahelps.ugetfirefoxwrapper.json --output-document /usr/lib/mozilla/native-messaging-hosts/com.javahelps.ugetfirefoxwrapper.json
+
+echo "uget-chrome-wrapper is installed successfully!"
+echo "Please install the 'uGet Integration' extension and restart the browser"
